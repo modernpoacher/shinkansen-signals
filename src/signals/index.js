db95e39bee4a94a619d4c4bfd65d5f00eace9aba
Jonathan Perry <jonathanperry@modernpoacher.com>
2021-09-18 17:53:19 +0100

    `yargs`

--- src/signals/index.js
+++ src/signals/index.js
@@ -1,28 +1,9 @@
 import debug from 'debug'
 
-import yargs from 'yargs'
-import {
-  hideBin
-} from 'yargs/helpers'
-
 import signals from './signals'
 
 const log = debug('shinkansen-signals')
 
 log('`shinkansen-signals` is awake')
 
-const {
-  argv = []
-} = process
-
-const filter = ([key, value]) => Reflect.has(signals, key)
-
-const reduce = (accumulator, [key, value]) => ({ ...accumulator, [key]: value })
-
-const SIGNALS = (
-  Object.entries(yargs(hideBin(argv)))
-    .filter(filter)
-    .reduce(reduce, {})
-)
-
-export default Object.freeze(Object.assign(signals, SIGNALS))
+export default Object.freeze(signals)
